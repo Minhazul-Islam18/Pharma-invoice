df3878eaed66cfb0f724f337faefcaaef22ad5f9
Minhazul Islam <minhaz019166@gmail.com>
2024-09-07 01:49:35 +0600

    Initial

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="c-footer">
+    <div>Pos © {{ date('Y') }} || Developed by <strong><a target="_blank" href="#">Sajjad Hossain</a></strong></div>
+
+    <div class="mfs-auto d-md-down-none">Version <strong class="text-danger">1.0</strong></div>
+</footer>
