296b0b8577a21b403542fd5565a1a0d85331b9aa
Minhazul Islam <minhaz019166@gmail.com>
2024-09-12 20:40:31 +0600

    Action buttons updated

--- Modules/Sale/Resources/views/partials/actions.blade.php
+++ Modules/Sale/Resources/views/partials/actions.blade.php
@@ -3,7 +3,7 @@
         <i class="bi bi-three-dots-vertical"></i>
     </button>
     <div class="dropdown-menu">
-        <a target="_blank" href="{{ route('sales.pos.pdf', $data->id) }}" class="dropdown-item">
+        {{-- <a target="_blank" href="{{ route('sales.pos.pdf', $data->id) }}" class="dropdown-item">
             <i class="bi bi-file-earmark-pdf mr-2 text-success" style="line-height: 1;"></i> POS Invoice
         </a>
         @can('access_sale_payments')
@@ -12,12 +12,12 @@
             </a>
         @endcan
         @can('access_sale_payments')
-            @if($data->due_amount > 0)
-            <a href="{{ route('sale-payments.create', $data->id) }}" class="dropdown-item">
-                <i class="bi bi-plus-circle-dotted mr-2 text-success" style="line-height: 1;"></i> Add Payment
-            </a>
+            @if ($data->due_amount > 0)
+                <a href="{{ route('sale-payments.create', $data->id) }}" class="dropdown-item">
+                    <i class="bi bi-plus-circle-dotted mr-2 text-success" style="line-height: 1;"></i> Add Payment
+                </a>
             @endif
-        @endcan
+        @endcan --}}
         @can('edit_sales')
             <a href="{{ route('sales.edit', $data->id) }}" class="dropdown-item">
                 <i class="bi bi-pencil mr-2 text-primary" style="line-height: 1;"></i> Edit
@@ -28,7 +28,7 @@
                 <i class="bi bi-eye mr-2 text-info" style="line-height: 1;"></i> Details
             </a>
         @endcan
-        @can('delete_sales')
+        {{-- @can('delete_sales')
             <button id="delete" class="dropdown-item" onclick="
                 event.preventDefault();
                 if (confirm('Are you sure? It will delete the data permanently!')) {
@@ -40,6 +40,6 @@
                     @method('delete')
                 </form>
             </button>
-        @endcan
+        @endcan --}}
     </div>
 </div>
